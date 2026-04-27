29dc648940fb6c367a1dc129d0f308c51e196bcd
Simone Dunbar <simone_dunbar@brown.edu>
2026-04-26 23:04:29 -0400

    changed button

--- components/home-page.jsx
+++ components/home-page.jsx
@@ -6,7 +6,7 @@ export default function HomePage({ onNavigate }) {
   return (
     <div className="max-w-6xl mx-auto px-6 py-12">
     
-      <button onClick={() => onNavigate("experience")} className="border-5 border-emerald-400 rounded-lg p-8 mb-8 shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 cursor-pointer">
+      <div className="border-5 border-emerald-400 rounded-lg p-8 mb-8 shadow-lg hover:shadow-2xl hover:-translate-y-2 transition-all duration-300 cursor-pointer">
       <div className="flex">
         <img src="laptop-typing.jpg" alt="Laptop Typing" className="w-1/2 rounded-lg mr-8" />
         <div>
@@ -17,16 +17,16 @@ export default function HomePage({ onNavigate }) {
             Take a deep dive into the world of ghost work and experience the challenges and realities faced by data workers. This interactive experience is designed to shed light on the often invisible labor that powers our digital world. Do you have what it takes to navigate the complexities of ghost work? Enter the experience and find out!
           </p>
           <div className="flex justify-center">
-            {/* <button
+            <button
               onClick={() => onNavigate("experience")}
               className="bg-emerald-400 text-neutral-950 px-10 py-2 rounded-full text-lg font-medium hover:bg-emerald-300 transition-colors"
             >
               Enter experience <ArrowRight className="inline-block ml-2" />
-            </button> */}
+            </button>
           </div>
           </div>
         </div>
-      </button>
+      </div>
 
       {/* Cards Section */}
       <div className="grid grid-cols-2 gap-6">
